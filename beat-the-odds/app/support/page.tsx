f601f2f2263e425068e6d231ea11408b3ed13340
Brian Tran <briantran@brians-mbp-3.unl.edu>
2024-02-18 04:13:11 -0600

    get data topics

--- beat-the-odds/app/support/page.tsx
+++ beat-the-odds/app/support/page.tsx
@@ -1,13 +1,19 @@
-"use client"
-import React, {useEffect, useState} from 'react';
-import Image from 'next/image';
-import UNO from '../images/UNOLogo.png';
-import UNL from '../images/UNLLogo.png';
-import UNK from '../images/UNKLogo.png';
+"use client";
+import React, { useEffect, useState } from "react";
+import Image, { StaticImageData } from "next/image";
+import UNO from "../images/UNOLogo.png";
+import UNL from "../images/UNLLogo.png";
+import UNK from "../images/UNKLogo.png";
+import { topicList } from "../interfaces/topicData";
 
-import SupportElement, { supportPage } from './supportElement'; // Assuming Support component is in the same directory
-import { useHealthContext } from '../context/healthContext';
+import SupportElement, { supportPage } from "./supportElement"; // Assuming Support component is in the same directory
+import { useHealthContext } from "../context/healthContext";
 
+interface ICollege {
+  name: string;
+  image: StaticImageData;
+  url: string;
+}
 const colleges = [
   {
     name: "University of Nebraska - Omaha",
@@ -26,94 +32,123 @@ const colleges = [
   },
 ];
 
-const CollegeTile = ({ name, image, url }) => {
+const CollegeTile = ({ name, image, url }: ICollege) => {
   return (
-    <a href={url} target="_blank" rel="noopener noreferrer" style={{ display: 'inline-block', textAlign: 'center', textDecoration: 'none', position: 'relative' }}>
-      <div style={{ width: '200px', height: '200px', margin: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)' }}>
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{
+        display: "inline-block",
+        textAlign: "center",
+        textDecoration: "none",
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          margin: "20px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+        }}
+      >
         <Image src={image} alt={name} width={175} height={175} />
       </div>
-      <div style={{ position: 'absolute', bottom: '-8px', left: '0', right: '0', height: '8px', background: 'linear-gradient(to bottom, rgba(0,0,0,0.2) 0%,rgba(0,0,0,0) 100%)' }}></div>
+      <div
+        style={{
+          position: "absolute",
+          bottom: "-8px",
+          left: "0",
+          right: "0",
+          height: "8px",
+          background:
+            "linear-gradient(to bottom, rgba(0,0,0,0.2) 0%,rgba(0,0,0,0) 100%)",
+        }}
+      ></div>
     </a>
   );
 };
 
-const options = 
-  [
-    { id: "531", title: "Get Screened" },
-    { id: "30533", title: "Get Enough Sleep" },
-    { id: "30537", title: "Get Your Cholesterol Checked" },
-    { id: "30538", title: "Choose the Right Birth Control" },
-    { id: "30539", title: "Eat Healthy" },
-    { id: "30540", title: "Get Your Well-Woman Visit Every Year" },
-    { id: "30541", title: "Men: Take Charge of Your Health" },
-    { id: "30546", title: "Take Steps to Prevent Type 2 Diabetes" },
-    { id: "30547", title: "Get Active" },
-    { id: "30558", title: "Drink Alcohol Only in Moderation" },
-    { id: "30560", title: "Manage Stress" },
-    { id: "30588", title: "Quitting Smoking: Conversation Starters" },
-    { id: "30604", title: "Depression: Conversation Starters" },
-    { id: "30606", title: "Alcohol Use: Conversation Starters" },
-    { id: "30610", title: "Quit Smoking" },
-    { id: "30614", title: "Testing for Syphilis: Questions for the Doctor" },
-    { id: "33303", title: "Talk with Your Doctor About Drug Misuse" },
-    { id: "33304", title: "Drug Misuse: Conversation Starters" },
-    { id: "34691", title: "Get Your Child Screened for Anxiety" },
-    { id: "34692", title: "Anxiety: Conversation Starters" }
-  ];
+export default function Support() {
+  const { topics } = useHealthContext();
+  const [selectedOption, setSelectedOption] = useState("");
 
-
-export default function Support(){
-  const { topics, myHealthFinder, itemList } = useHealthContext();
-  const [selectedOption, setSelectedOption] = useState('');
-
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     console.log("Selected option:", selectedOption);
   };
 
-  const handleSelectChange = (e) => {
+  const handleSelectChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
     setSelectedOption(e.target.value);
   };
 
   useEffect(() => {
     console.log(topics);
-    // console.log(myHealthFinder);
-    // console.log(itemList);
-  }, [topics, myHealthFinder, itemList]);
+  }, [topics]);
   return (
     <>
       <div className="jonah-spacer"></div>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        {colleges.map(college => (
-          <CollegeTile key={college.name} name={college.name} image={college.image} url={college.url} />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {colleges.map((college) => (
+          <CollegeTile
+            key={college.name}
+            name={college.name}
+            image={college.image}
+            url={college.url}
+          />
         ))}
       </div>
 
       <div className="mt-20">
         <form onSubmit={handleSubmit} className="flex gap-2 flex-col">
-          <select name="title" value={selectedOption} onChange={handleSelectChange} className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-with:border-slate-100">
+          <select
+            name="title"
+            value={selectedOption}
+            onChange={handleSelectChange}
+            className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-with:border-slate-100"
+          >
             <option value="">Select...</option>
-            {options.map((option) => (
-              <option key={option.id} value={option.id}>{option.title}</option>
+            {topicList.map((topic) => (
+              <option key={topic.id} value={topic.id}>
+                {topic.title}
+              </option>
             ))}
           </select>
           <div className="flex gap-1 justify-end">
-            <button type="submit" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">Submit</button>
+            <button
+              type="submit"
+              className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+            >
+              Submit
+            </button>
           </div>
         </form>
-      <SupportElement
-        title="Support Title"
-        link="https://www.amazon.com/"
-        imageUrl="https:\/\/health.gov\/sites\/default\/files\/2023-03\/Teen-screen-depression_1194319414.jpg"
-      />
-      {/* <SupportElement
+        {topics.map((topic) => (
+          <SupportElement
+            key={topic.Id}
+            title={topic.Title}
+            link={topic.AccessibleVersion}
+            imageUrl={topic.ImageUrl}
+          />
+        ))}
+        {/* <SupportElement
+          title="Support Title"
+          link="https://www.amazon.com/"
+          imageUrl="https:\/\/health.gov\/sites\/default\/files\/2023-03\/Teen-screen-depression_1194319414.jpg"
+        /> */}
+        {/* <SupportElement
         key={index}
         title={resources.title}
         Link={resources.url}
         imageUrl="https://example.com/image.jpg"
       /> */}
-    </div>
+      </div>
     </>
   );
-};
-   
+}
--- beat-the-odds/app/support/supportElement.tsx
+++ beat-the-odds/app/support/supportElement.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-export interface supportPage  {
-    title: string;
-    link: string;
-    imageUrl: string;
+export interface supportPage {
+  title: string;
+  link: string;
+  imageUrl: string;
 }
 
-export function SupportElement ({ title, link, imageUrl}: supportPage) {
+export function SupportElement({ title, link, imageUrl }: supportPage) {
   return (
     <div className="flex items-center justify-between space-x-4 p-4 border rounded-md shadow-md">
-    <div>
+      <div>
+        <h2 className="text-xl font-bold">{title}</h2>
 
-      <h2 className="text-xl font-bold">{title}</h2>
+        <Link href={link} className="mt-2">
+          {link}
+        </Link>
+      </div>
 
-      <p className="mt-2">{link}</p>
-    </div>
-
-      <img src={imageUrl} alt="Support Image" className="w-32 h-32 object-cover rounded-md" />
+      <img
+        src={imageUrl}
+        alt="Support Image"
+        className="w-32 h-32 object-cover rounded-md"
+      />
     </div>
   );
 }
