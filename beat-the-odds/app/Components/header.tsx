45a9f45263bf2e815b94738418a8b158f0bd8237
sanjay <sanjaygirip@gmail.com>
2024-02-17 20:07:28 -0600

    sticky header

--- beat-the-odds/app/Components/header.tsx
+++ beat-the-odds/app/Components/header.tsx
@@ -1,13 +1,20 @@
 import Link from "next/link";
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
-export default async function Header(){
-    return <>
-    <header className="flex justify-between items-center mb-4">
-        <h1 className="text-3xl">StudentSupportNet</h1>
-        <Link href="/new"
-        className="border border-slate-300 text-slate-300 px-2 py-1 rounded 
-        hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-        >Get Started</Link>
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center mb-4 p-4 header fixed w-full top-0 z-50" style={{ backgroundColor: 'var(--green)' }}>
+      <div className="flex">
+        <img src="/favicon.ico" alt="Favicon" className="h-11 mr-2" />
+        <h1 className="text-3xl text-black">StudentSupportNet</h1>
+      </div>
+      <div className="flex">
+        <a href="/" className="mr-4 text-black px-4 py-2">Services</a>
+        <a href="/" className="border border-black text-black px-4 py-2 rounded hover:bg-green-700 focus:bg-green-700">
+          Get Started
+        </a>
+      </div>
+
     </header>
-    </>
+  );
 }
--- beat-the-odds/app/page.tsx
+++ beat-the-odds/app/page.tsx
@@ -2,15 +2,13 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Chart from "./components/chart";
+import Mission from "./components/mission";
 
 export default function Home() {
   return <>
     <Header/>
     <Chart/>
-    <div className="max-w-2xl px-2">
-      <h1 className="text-3xl font-bold mb-4">Our Mission</h1>
-      <p className="text-lg mb-4">Our mission at Student Support Net is to empower individuals to beat the odds and thrive in their mental health journey. In a world where the challenges of everyday life can sometimes feel insurmountable, we believe in providing a supportive online platform where college students can find solace, connection, and resources to navigate their mental health struggles. By fostering a community of empathy, understanding, and resilience, we aim to break down barriers, challenge stigma, and inspire hope. Together, we strive to create a space where every individual feels valued, supported, and equipped with the tools they need to overcome adversity and embrace their full potential.</p>
-    </div>
+    <Mission/>
     <Footer/>
   </>
     
