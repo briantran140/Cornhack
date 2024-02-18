cf3b0eab05be3fe77374e209cef1c6ef0f1ae737
Luke Irwin <lukemirwin@gmail.com>
2024-02-18 01:18:41 -0600

    i hate this game

--- beat-the-odds/app/components/header.tsx
+++ beat-the-odds/app/components/header.tsx
@@ -1,20 +1,28 @@
 import Link from "next/link";
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center mb-4 p-4 header fixed w-full top-0 z-50" style={{ backgroundColor: 'var(--green)' }}>
-      <div className="flex">
+    <header
+      className="flex justify-between items-center mb-4 p-4 header fixed w-full top-0 z-50"
+      style={{ backgroundColor: "var(--green)" }}
+    >
+      <a href="/" className="flex">
         <img src="/favicon.ico" alt="Favicon" className="h-11 mr-2" />
         <h1 className="text-3xl text-black">StudentSupportNet</h1>
-      </div>
+      </a>
       <div className="flex">
-        <a href="/about" className="mr-4 text-black px-4 py-2">About Us</a>
-        <a href="/support" className="border border-black text-black px-4 py-2 rounded hover:bg-green-700 focus:bg-green-700 hover:underline;">
+        <a href="/about" className="mr-4 text-black px-4 py-2 hover:underline">
+          About Us
+        </a>
+
+        <a
+          href="/support"
+          className="border border-black text-black px-4 py-2 rounded hover:bg-green-700 focus:bg-green-700"
+        >
           Get Support
         </a>
       </div>
-
     </header>
   );
 }
